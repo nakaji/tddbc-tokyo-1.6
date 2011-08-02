2e881fa39416d8b3c64a9916c3a5c6d3c7d1496e
nakaji <nakaji126@gmail.com>
2011-08-02 22:41:27 +0900

    T16MAIN-1: putでkeyとvalueを追加し、dumpで一覧表示、getでkeyに対応するvalueを取得できる
    ・put で keyとvalueを追加する
    ・dumpで登録されている一覧を表示
    ・getで指定したkeyのvalueを取得
    注1)keyにnullをわたすと例外が発生
    注2)valueのnullは許容する

--- TDDBC/T16Main.cs
+++ TDDBC/T16Main.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace TDDBC
 {
     public class T16Main
     {
+        private Dictionary<string,string> _set = new Dictionary<string,string>();
+
+        public void Put(string key,string value)
+        {
+            _set.Add(key, value);
+
+        }
+
+        public string Get(string key)
+        {
+            return _set[key];
+        }
+
+        public string Dump()
+        {
+            string result = "";
+            _set.ToList().ForEach(x => { result += x.Value + "\n"; });
+            return result;
+        }
     }
 }
--- TDDBCTest/T16MainTest.cs
+++ TDDBCTest/T16MainTest.cs
@@ -1,14 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using TDDBC;
 
 namespace TDDBCTest
 {
     [TestFixture]
     public class T16MainTest
     {
+        [Test]
+        public void Getで指定したkeyのvalueを取得()
+        {
+            var t16Main = new T16Main();
+            t16Main.Put("AA", "999");
+            string result = t16Main.Get("AA");
 
+            Assert.That(result, Is.EqualTo("999"));
+        }
+
+        [Test]
+        public void Dumpで登録されている一覧を表示()
+        {
+            var t16Main = new T16Main();
+            t16Main.Put("AA", "999");
+            t16Main.Put("ZZ", "111");
+            string result = t16Main.Dump();
+
+            Assert.That(result, Is.EqualTo("999\n111\n"));
+        }
+        
+        [Test]
+        [ExpectedException("System.ArgumentNullException")]
+        public void keyにnullをわたすと例外が発生()
+        {
+            var t16Main = new T16Main();
+            t16Main.Put(null, "999");
+        }
+
+        [Test]
+        public void valueのnullは許容する()
+        {
+            var t16Main = new T16Main();
+            t16Main.Put("hoge", null);
+        }
     }
 }
